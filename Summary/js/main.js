8711c6b917e5ec17ed7dbbbd91ba8f0f70e8882d
AntonPoloumov13 <poloumov13@mail.ru>
2023-04-11 18:51:03 +0400

    index

--- Summary/js/main.js
+++ Summary/js/main.js
@@ -197,21 +197,21 @@ da.init();
 }
 //
 
-//function onEntry(entry) {
-//  entry.forEach(change => {
- //   if (change.isIntersecting) {
- //    change.target.classList.add('element-show');
-//    } else  {
-//      change.target.classList.remove('element-show');
-//    }
-//  });
-//}
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    } else  {
+      change.target.classList.remove('element-show');
+    }
+  });
+}
 
-//let options = {
- // threshold: [0.5] };
-//let observer = new IntersectionObserver(onEntry, options);
-//let elements = document.querySelectorAll('section');
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('section');
 
-//for (let elm of elements) {
- // observer.observe(elm);
-//}
+for (let elm of elements) {
+  observer.observe(elm);
+}
