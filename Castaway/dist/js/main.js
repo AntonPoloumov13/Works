bf49f7ef6109bdb0165d641a4d4e47a1430bbd7a
AntonPoloumov13 <poloumov13@mail.ru>
2023-06-24 18:16:47 +0400

    index

---
+++ Castaway/dist/js/main.js
@@ -0,0 +1,178 @@
+"use strict"
+
+window.onload = function () {
+
+  const menu = document.querySelector(".header__menu");
+
+document.addEventListener("click", function (e) {
+  if(e.target.closest(".burger")) {
+    menu.classList.add("menu-active")
+    document.body.style.overflow = "hidden";
+  }
+  if (e.target.closest(".header__exit-menu")) {
+    menu.classList.remove("menu-active")
+    document.body.style.overflow = "";
+  }
+  if (e.target.closest(".nav__link")) {
+    menu.classList.remove("menu-active")
+    document.body.style.overflow = "";
+  }
+})
+  
+const form = document.querySelector(".about__bottom-form")
+if(form)  {
+  new JustValidate('.about__form', {
+  colorWrong: "#FF6972",
+  rules: {
+    name: {
+      required: true,
+      minLength: 2,
+      strength: {
+        custom: '[а-яА-Я\d]',
+      },
+    },
+    tel: {
+      required: true,
+      function: (name, value) => {
+        const phone = selector.inputmask.unmaskedvalue()
+        return Number(phone) && phone.length === 10
+      }
+    },
+  },
+  messages: {
+    name: {
+      required: 'Введите ваше имя',
+      minLength: 'Минимальное количество букв - 2',
+      strength: 'Недопустимый формат',
+    },
+    tel: 'Введите ваш телефон'
+  },
+
+
+  // Отправка на почту
+  submitHandler: function (form, values, ajax) {
+    let formData = new FormData(form);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function () {
+
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log("Отправлено")
+        }
+      }
+    }
+
+    xhr.open("POST", "mail.php", true);
+    xhr.send(formData)
+
+    form.reset();
+  }
+});
+
+}
+const inputName = document.querySelector(".input-name");
+if(inputName) {
+  let banNumber = /[0-9]/g;
+  let banEnglishLetter = /[^a-zA-ZА-Яа-яЁё]/g;
+  inputName.oninput = function () {
+  this.value = this.value.replace(banNumber, "")
+  this.value = this.value.replace(banEnglishLetter, "")
+}
+inputName.addEventListener("change", function () {
+  let length = inputName.value.length
+  if (length >= 0) {
+    document.querySelector(".input-name").classList.add("form-label-done");
+  } else {
+    document.querySelector(".input-name").classList.remove("form-label-done");
+  }
+  if (document.querySelectorAll(".form-label-done").length == 2) {
+    document.querySelector(".about__form-btn").classList.remove("disabled")
+  }
+})
+document.querySelector(".input-mail").addEventListener("change", function () {
+  let length = inputName.value.length
+  if (length >= 0) {
+    document.querySelector(".input-mail").classList.add("form-label-done");
+  } else {
+    document.querySelector(".input-mail").classList.remove("form-label-done");
+  }
+  if (document.querySelectorAll(".form-label-done").length == 2) {
+    document.querySelector(".about__form-btn").classList.remove("disabled")
+  }
+})
+}
+
+/* Обязательно инпутам добавить name и проверь пути к mail.php в js и html
+
+
+<form action="mail.php" class="contacts__form" method="post" enctype="multipart/form-data">
+<h3 class="contacts__form-subtitle">Заказать обратный звонок</h3>
+<input type="hidden" name="admin_email[]" value="exigonyashka@yandex.ru">
+<input type="hidden" name="form_subject" value="Заявка с сайта">
+<label class="form-label margin-top"><input type="text" name="name" placeholder="Имя*" required
+    data-validate-field="name" id="name" aria-label="Введите своё имя"></label>
+<label class="form-label"><input type="tel" name="phone" placeholder="Телефон*" required
+    data-validate-field="tel" data-validate-rules="phone" id="tel"
+    aria-label="Введите свой телефон"></label>
+<button class="contacts__form-btn" type="submit">Заказать</button>
+</form>
+
+
+*/
+
+  const headerElement = document.querySelector(".header")
+
+const callback = function (entries, observer) {
+  if (entries[0].isIntersecting) {
+    headerElement.classList.remove("scroll")
+  } else {
+    headerElement.classList.add("scroll")
+  }
+}
+
+const headerObserver = new IntersectionObserver(callback);
+headerObserver.observe(headerElement);
+
+/* 
+    Рекомендуемое строение
+
+ <div class="header">
+  <div class="header__wrapper">
+    <ul>
+        <li>
+            <a href="#">adsadsadsads</a>
+        </li>
+        <li>
+            <a href="#">adsadsadsads</a>
+        </li>
+        <li>
+            <a href="#">adsadsadsads</a>
+        </li>
+        <li>
+            <a href="#">adsadsadsads</a>
+        </li>
+    </ul>
+  </div>
+</div> 
+*/
+
+
+
+}
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
